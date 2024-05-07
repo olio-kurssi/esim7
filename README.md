215a330cd1d38a67ddbc29c0092f18466ceedba5
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-05-07 13:36:15 +0300

    esimerkki7 vectorilla

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # esim7 Oliotaulukko
 
-Tässä esimerkissä **mainissa** luodaan taulukko nimeltään **studentList** ja siihen lisätään 5 Student luokan oliota.
+Tässä esimerkissä **mainissa** luodaan vektori nimeltään **studentList** ja siihen lisätään 5 Student luokan oliota.
 
 Student luokalle on lisätty construktori, jolla voidaan oliota luotaessa antaa nimi, syntymävuosi ja ryhmä.
 Luokalle on lisätty myös metodi **printStudentData**, jonka avulla kaikki kolme edellä mainittua saadaan tulostettua ruudulle.
--- main.cpp
+++ main.cpp
@@ -3,27 +3,33 @@
 
 #include <iostream>
 #include <vector>
-
+#include <memory>
 
 using namespace std;
 
 int main()
 {
-    vector<Student> studentList;
+    unique_ptr<vector<Student>> studentList = make_unique<vector<Student>>();
 
     Student objectStudent0("Teppo Testi",1999,"TVT23SPL");
-    studentList.push_back(objectStudent0);
+    studentList->push_back(objectStudent0);
     Student objectStudent1("Liisa Joki",1998,"TVT23SPL");
-    studentList.push_back(objectStudent1);
+    studentList->push_back(objectStudent1);
     Student objectStudent2("Aino Virta",1997,"TVT23SPO");
-    studentList.push_back(objectStudent2);
+    studentList->push_back(objectStudent2);
     Student objectStudent3("Matti Virtanen",2001,"TVT23SPO");
-    studentList.push_back(objectStudent3);
+    studentList->push_back(objectStudent3);
     Student objectStudent4("Mikko Vilkas",2001,"TVT23SPL");
-    studentList.push_back(objectStudent4);
+    studentList->push_back(objectStudent4);
 
     for(int x=0; x<=4; x++){
-        studentList[x].printStudentData();
+        // Hae opiskelijalista studentList-osoittimesta
+        vector<Student>& students = *studentList;
+
+        // Käytä opiskelijalistan elementtiä `x` ja kutsu printStudentData() -funktiota
+        students[x].printStudentData();
     }
+
+
     return 0;
 }
